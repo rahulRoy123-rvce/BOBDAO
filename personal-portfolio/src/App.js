f2ec24b64521ac5ffc451b8ba2f4d40c4700891e
Rahul Roy <rahulroy.ec21@rvce.edu.in>
2022-08-28 05:39:51 +0530

    unsd

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -7,7 +7,7 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
-import { UNSD } from "./components/UNSD";
+import { UNSD } from "../src/components/UNSD";
 
 function App() {
   return (
